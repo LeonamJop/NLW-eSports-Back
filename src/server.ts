65ab3ed996f3215399ee9b71e840faca0bbddbd7
Leonam Silva <leonam.murilo@gmail.com>
2022-09-14 13:55:57 -0300

    Preparando ambiente back

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const app = express()
+
+app.get('/ads', (request, reponse) => {
+    return reponse.json([
+        {
+            id: 1,
+            nome: "Anúncio 1",
+        },{
+            id: 2,
+            nome: "Anúncio 2",
+        },{
+            id: 3,
+            nome: "Anúncio 3",
+        },{
+            id: 4,
+            nome: "Anúncio 4",
+        },
+    ])
+});
+
+app.listen(3333)
